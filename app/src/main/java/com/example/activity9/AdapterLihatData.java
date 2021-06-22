d3f723e876c1d1635b5c9cf89d420c2fa3f2132f
SisiliaIndah18 <79432738+SisiliaIndah18@users.noreply.github.com>
2021-06-22 15:02:58 +0700

    lihat data

--- app/src/main/java/com/example/activity9/AdapterLihatData.java
+++ app/src/main/java/com/example/activity9/AdapterLihatData.java
@@ -1,4 +1,58 @@
 package com.example.activity9;
 
-public class AdapterLihatData {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class AdapterLihatData extends RecyclerView.Adapter<AdapterLihatData.ViewHolder> {
+
+    private ArrayList<Barang> daftarBarang;
+    private Context context;
+    public AdapterLihatData(ArrayList<Barang> barangs, Context ctx){
+        daftarBarang = barangs;
+        context = ctx;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView tvTitle;
+
+        public ViewHolder(View v) {
+            super(v);
+            tvTitle = (TextView) v.findViewById(R.id.tv_namabarang);
+        }
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_data, parent,false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        final String name = daftarBarang.get(position).getNama();
+        holder.tvTitle.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                //next act untuk fungsi detail data
+            }
+        });
+        holder.tvTitle.setText(name);
+    }
+
+    @Override
+    public int getItemCount() {
+        //mengembalikan jumlah item pada barang
+        return daftarBarang.size();
+    }
 }
--- app/src/main/java/com/example/activity9/LihatData.java
+++ app/src/main/java/com/example/activity9/LihatData.java
@@ -1,4 +1,63 @@
 package com.example.activity9;
 
-public class LihatData {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class LihatData extends AppCompatActivity {
+
+    private DatabaseReference database;
+    private RecyclerView rvView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<Barang> daftarBarang;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lihat_data);
+
+        rvView = (RecyclerView) findViewById(R.id.rv_main);
+        rvView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        rvView.setLayoutManager(layoutManager);
+
+        database = FirebaseDatabase.getInstance().getReference();
+        database.child("Barang").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                daftarBarang = new ArrayList<>();
+                for (DataSnapshot noteDataSnapshot : dataSnapshot.getChildren()){
+                    Barang barang = noteDataSnapshot.getValue(Barang.class);
+                    barang.setKode(noteDataSnapshot.getKey());
+                    daftarBarang.add(barang);
+                }
+
+                adapter = new AdapterLihatData(daftarBarang, LihatData.this);
+                rvView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                System.out.println(databaseError.getDetails()+""+databaseError.getMessage());
+            }
+        });
+    }
+    public static Intent getActIntent(Activity activity){
+        return new Intent(activity, LihatData.class);
+    }
 }
